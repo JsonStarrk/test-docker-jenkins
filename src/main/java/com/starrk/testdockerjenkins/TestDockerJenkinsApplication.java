eb901acab0a1c063786c3e05cf243c305314a119
N8O <463479101@qq.com>
2021-03-27 09:23:50 +0800

    Initial commit

---
+++ src/main/java/com/starrk/testdockerjenkins/TestDockerJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.starrk.testdockerjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDockerJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDockerJenkinsApplication.class, args);
+    }
+
+}
